ec161bf5bcb1e0939b4722c61c6a77e4cf8acee5
clarrexd <clarre.renstrom@gmail.com>
2024-01-07 19:50:21 +0100

    login window finalized stylewise. Added google login button

--- Webshop/frontend/src/app/login/login.component.ts
+++ Webshop/frontend/src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import {
 } from '@fortawesome/angular-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -16,5 +17,6 @@ import { faLock } from '@fortawesome/free-solid-svg-icons';
 export class LoginComponent {
   faUser = faUser;
   faLock = faLock;
+  faGoogle = faGoogle;
   constructor(library: FaIconLibrary) {}
 }

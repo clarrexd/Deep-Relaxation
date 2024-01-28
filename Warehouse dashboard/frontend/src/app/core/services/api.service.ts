93c1112b1267aecc55e73a7dd4197b57fb5532da
clarrexd <clarre.renstrom@gmail.com>
2024-01-28 13:58:41 +0100

    html template to show orders from GET request

--- Warehouse dashboard/frontend/src/app/core/services/api.service.ts
+++ Warehouse dashboard/frontend/src/app/core/services/api.service.ts
@@ -9,33 +9,22 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
-  //Variable to hold all orders in an array or a specific order for order details
-  ordersList: any;
-
   //Variable to hold all products in the inventory in an array
   inventoryList: any;
 
-  getAllOrders() {
-    this.http
-      .get('http://localhost:9000/warehouse/orders')
-      .subscribe((response) => {
-        this.ordersList = response;
-      });
-  }
+  // getOrderDetail(id: any) {
+  //   this.http
+  //     .get(`http://localhost:9000/warehouse/orders/${id}`)
+  //     .subscribe((response) => {
+  //       return response;
+  //     });
+  // }
 
-  getOrderDetail(id: any) {
-    this.http
-      .get(`http://localhost:9000/warehouse/orders/${id}`)
-      .subscribe((response) => {
-        this.ordersList = response;
-      });
-  }
-
-  getInventory() {
-    this.http
-      .get('http://localhost:9000/warehouse/inventory')
-      .subscribe((response) => {
-        this.inventoryList = response;
-      });
-  }
+  // getInventory() {
+  //   this.http
+  //     .get('http://localhost:9000/warehouse/inventory')
+  //     .subscribe((response) => {
+  //       this.inventoryList = response;
+  //     });
+  // }
 }
--- Warehouse dashboard/frontend/src/app/pages/orders/orders.component.ts
+++ Warehouse dashboard/frontend/src/app/pages/orders/orders.component.ts
@@ -1,11 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../core/services/api.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.scss',
 })
-export class OrdersComponent {}
+export class OrdersComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  //Variable to hold all orders
+  ordersList: any;
+
+  getAllOrders() {
+    this.http
+      .get('http://localhost:9000/warehouse/orders')
+      .subscribe((response) => {
+        this.ordersList = response;
+      });
+  }
+
+  ngOnInit(): void {
+    this.ordersList = this.getAllOrders();
+    console.log(this.ordersList);
+  }
+}

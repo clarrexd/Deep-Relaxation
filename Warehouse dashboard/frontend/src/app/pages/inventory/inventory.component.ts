1ff74ac2e84aa0030914d758982b926fa85db207
clarrexd <clarre.renstrom@gmail.com>
2024-01-30 09:51:13 +0100

    Stock balance can now be edited in the inventory tab

--- Warehouse dashboard/frontend/src/app/pages/inventory/inventory.component.ts
+++ Warehouse dashboard/frontend/src/app/pages/inventory/inventory.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inventory',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './inventory.component.html',
   styleUrl: './inventory.component.scss',
 })
@@ -17,12 +18,41 @@ export class InventoryComponent implements OnInit {
   fetchInventory() {
     this.http
       .get('http://localhost:9000/warehouse/inventory')
-      .subscribe((response) => {
-        this.inventoryList = response;
+      .subscribe((response: any) => {
+        this.inventoryList = response.map((product: any) => ({
+          ...product,
+          editMode: false,
+          updatedStockBalance: product.stock_balance,
+        }));
       });
   }
 
+  startEditMode(product: any): void {
+    // Set editMode to true for the clicked product
+    product.editMode = true;
+  }
+
+  saveChanges(product: any): void {
+    //PATCH request to update the stock balance for the clicked product
+    this.http
+      .patch('http://localhost:9000/warehouse/inventory', {
+        id: product.id,
+        stock_balance: product.updatedStockBalance,
+      })
+      .subscribe(() => {
+        console.log('Stock balance updated successfully.');
+        // Update local data after successful save
+        product.stock_balance = product.updatedStockBalance;
+        product.editMode = false; // Exit edit mode
+      });
+  }
+
+  cancelEditMode(product: any): void {
+    // Cancel the ongoing edit mode for the clicked product
+    product.editMode = false;
+  }
+
   ngOnInit(): void {
-    this.inventoryList = this.fetchInventory();
+    this.fetchInventory();
   }
 }

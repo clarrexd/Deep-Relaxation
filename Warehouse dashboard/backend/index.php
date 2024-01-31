0ac952dfd5fe20583be32ceed3d483981fcff4c8
clarrexd <clarre.renstrom@gmail.com>
2024-01-31 19:21:11 +0100

    header change php backend

--- Warehouse dashboard/backend/index.php
+++ Warehouse dashboard/backend/index.php
@@ -6,7 +6,11 @@ require_once __DIR__ . '/controllers/orderDetailsController.php';
 
 
 
-header('Content-Type: application/json');
+// header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
 
 
 $URI = $_SERVER['REQUEST_URI'];

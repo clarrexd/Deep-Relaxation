cfcaa095df043c1258aed921eee94bea52a83770
clarrexd <clarre.renstrom@gmail.com>
2024-02-01 18:19:51 +0100

    documentation

--- Warehouse dashboard/backend/index.php
+++ Warehouse dashboard/backend/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/controllers/ordersController.php';
 require_once __DIR__ . '/controllers/orderDetailsController.php';
 
 
-
+//CORS headers
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
@@ -17,6 +17,7 @@ header("Access-Control-Allow-Credentials: true");
 $URI = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+//Handling CORS pre-flight request
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
